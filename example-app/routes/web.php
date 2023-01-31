3c4fd740963279dd91874011ba44c5d4343ad034
Ariful <websiteobuilder@gmail.com>
2023-02-01 00:35:26 +0600

    some change

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -12,7 +12,14 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+route::get('/',function(){
+    return view('home');
+});
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/ariful', function () {
+    return ('welcome ariful');
+});
+
+route::get('/samira',function(){
+    return('welcome samira');
 });
